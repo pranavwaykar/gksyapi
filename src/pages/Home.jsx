8b03c2de3c39d8142ab17697ffd34e45d01fb558
Pranav Waykar <PWaykar@nextsmartmove.ai>
2025-04-01 11:42:59 +0530

    # Please enter the commit message for your changes. Lines starting # with '#' will be ignored, and an empty message aborts the commit. # # On branch main # Your branch is up to date with 'origin/main'. # # Changes to be committed: #	modified:   src/pages/Home.jsx #	modified:   src/styles/main.scss #	new file:   src/styles/pages/_home.scss #	modified:   src/styles/pages/_projects.scss #	new file:   src/styles/pages/projects.scss # feat(projects): implement fullscreen projects slider
    - Add fullscreen project slider with navigation
    - Implement smooth transitions and animations
    - Add project title and description overlays
    - Include hover effects and gradient overlays
    - Add show more button with arrow animation
    - Configure project navigation controls

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,52 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Home = () => {
   return (
-    <div className="page">
-      <div className="container">
-        <h1>Welcome to Our Company</h1>
-        <p>We provide innovative solutions for your business needs.</p>
+    <div className="home-container">
+      {/* Top Navigation */}
+      {/* <nav className="top-nav">
+        <div className="logo">ONE</div>
+        <ul>
+          <li><Link to="/">HOME</Link></li>
+          <li><Link to="/virtual-tour">THE VIRTUAL TOUR</Link></li>
+          <li><Link to="/gallery">THE GALLERY</Link></li>
+          <li><Link to="/video">THE VIDEO</Link></li>
+          <li><Link to="/details">THE DETAILS</Link></li>
+          <li><Link to="/press">THE PRESS</Link></li>
+          <li><Link to="/contact">THE CONTACT</Link></li>
+        </ul>
+      </nav> */}
+
+      {/* Main Content */}
+      <div className="main-content">
+        {/* Left Title Section */}
+        <div className="title-section">
+          <h1>
+            THE<br />
+            PALMS<br />
+            MOST<br />
+            LUXURIOUS<br />
+            VILLA<br />
+            TO<br />
+            RENT
+          </h1>
+          <p className="copyright">COPYRIGHT © 2022. ALL RIGHTS RESERVED.</p>
+        </div>
+
+        {/* Center Line with Text */}
+        <div className="vertical-text center">
+          <span className="small-text">ONE 100 PALM</span>
+          <div className="line"></div>
+          <span className="large-text">THE GALLERY</span>
+        </div>
+
+        {/* Right Line with Text */}
+        <div className="vertical-text right">
+          <span className="small-text">CLICK HERE FOR</span>
+          <div className="line"></div>
+          <span className="large-text">THE DETAILS</span>
+        </div>
       </div>
     </div>
   );
